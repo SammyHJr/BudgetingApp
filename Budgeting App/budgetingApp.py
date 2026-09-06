b76eea5b1a97d618105e90de01833c24dbbd8197
Sammy H. jr <132605016+SammyHJr@users.noreply.github.com>
2026-09-06 23:36:45 +0200

    Implemeted more functions
    Implemented transportation percentages savings and others

--- Budgeting App/budgetingApp.py
+++ Budgeting App/budgetingApp.py
@@ -5,17 +5,34 @@ yearlySalary = monthlySalary*12
 
 def rentPercentage(monthlySalary):
     rent = monthlySalary * 0.35
-    return rent
+    return round(rent, 5)
 
 def groceriesPercentage(monthlySalary):
     groceries = monthlySalary * 0.15
     return groceries
 
+def transportationPercentage(monthlySalary):
+    transportation = monthlySalary * 0.15
+    return transportation
+
+def savingsPercentage(monthlySalary):
+    savings = monthlySalary * 0.20
+    return savings
+
+def othersPercentage(monthlySalary):
+    others = monthlySalary * 0.15
+    return others
+
 maximumRent = rentPercentage(monthlySalary)
 maximumGroceries = groceriesPercentage(monthlySalary)
+maximumTransportation = transportationPercentage(monthlySalary)
+minimumSavings = savingsPercentage(monthlySalary)
+othersCost = othersPercentage(monthlySalary)
 
 print("Monthly Salary:", monthlySalary)
 print("Yearly Salary:", yearlySalary)
 print("Monthly maximum rent:", maximumRent)
 print("Cost of Groceries:", maximumGroceries)
-
+print("Cost of transporation:", maximumTransportation)
+print("Monthly Savings:", minimumSavings)
+print("Others: ", othersCost)
